6481ae754f54e844755bab955325e6354e3761d8
Akshay Maru <akshaymaru@Urami.local>
2021-08-23 13:15:04 +0530

    changes

---
+++ src/components/Components.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Header = styled.header
+`
+display: flex;
+width : 100%;
+justify-content : space-between;
+align-items : center;
+flex-flow : row wrap;
+
+.logo{
+    font-size : 26px;
+    font-weight : 100;
+}
+`
